4cf9d60aecc9eb2c63071307fd0b4725f61506e8
Preyash Patel <preyashecosense@gmail.com>
2023-05-18 19:08:40 +0530

    auctions table name made dynamic

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -16,7 +16,7 @@ async function createAuction(event, context) {
 
   await dynamodb
     .put({
-      TableName: "Auctions",
+      TableName: process.env.AUCTIONS_TABLE_NAME,
       Item: auction,
     })
     .promise();
