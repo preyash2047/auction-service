6f94bdcd8a3731f0e0290c48c4689c3133b4f507
Preyash Patel <preyashecosense@gmail.com>
2023-05-24 19:46:32 +0530

    update

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,9 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
 import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
+// import httpJsonBodyParser from "@middy/http-json-body-parser";
+// import httpEventNormalizer from "@middy/http-event-normalizer";
+// import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -37,7 +37,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = middy(createAuction);
+// .use(httpJsonBodyParser())
+// .use(httpEventNormalizer())
+// .use(httpErrorHandler());
