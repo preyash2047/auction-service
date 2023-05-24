725ce9a282a8e2c085163c10b2d90a3786426d7e
Preyash Patel <preyashecosense@gmail.com>
2023-05-24 21:31:27 +0530

    get auction

---
+++ src/handlers/getAuction.js
@@ -0,0 +1,33 @@
+import AWS from "aws-sdk";
+import commonMiddleware from "../lib/commonMiddleware";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuction(event, context) {
+  const { id } = event.pathParameters;
+  let auction;
+  try {
+    const result = await dynamodb
+      .get({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+      })
+      .promise();
+    auction = result.Item;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  if (!auction) {
+    throw new createError.NotFound(`Auction with ID "${id}" not found!`);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMiddleware(getAuction);
